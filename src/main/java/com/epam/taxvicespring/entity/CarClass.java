7a260c37a463f4c27c62d0477cd0a3c918c72802
skolach <ssskolach@gmail.com>
2021-11-29 11:13:55 +0200

    refactoring

--- src/main/java/com/epam/taxvicespring/entity/CarClass.java
+++ src/main/java/com/epam/taxvicespring/entity/CarClass.java
@@ -3,10 +3,6 @@ package com.epam.taxvicespring.entity;
 import java.io.Serializable;
 
 public class CarClass implements Serializable{
-    // id INT NOT NULL AUTO_INCREMENT,
-    // `name` VARCHAR(10) NOT NULL,
-    // `rate` SMALLINT NOT NULL,
-    // `description` VARCHAR(100),
     private int id;
     private String name;
     private int rate;
--- src/main/java/com/epam/taxvicespring/utils/UrlPatternUtils.java
+++ src/main/java/com/epam/taxvicespring/utils/UrlPatternUtils.java
@@ -26,40 +26,10 @@ public class UrlPatternUtils {
         return false;
     }
 
-    // servletPath:
-    // ==> /spath
-    // ==> /spath/*
-    // ==> *.ext
-    // ==> /
     public static String getUrlPattern(HttpServletRequest request) {
         ServletContext servletContext = request.getServletContext();
         String servletPath = request.getServletPath();
         String pathInfo = request.getPathInfo();
         return servletPath;
-
-        // String urlPattern = null;
-        // if (pathInfo != null) {
-        //     urlPattern = servletPath + "/*";
-        //     return urlPattern;
-        // }
-
-        // urlPattern = servletPath;
-
-        // boolean has = hasUrlPattern(servletContext, urlPattern);
-        // if (has) {
-        //     return urlPattern;
-        // }
-
-        // int i = servletPath.lastIndexOf('.');
-        // if (i != -1) {
-        //     String ext = servletPath.substring(i + 1);
-        //     urlPattern = "*." + ext;
-        //     has = hasUrlPattern(servletContext, urlPattern);
-
-        //     if (has) {
-        //         return urlPattern;
-        //     }
-        // }
-        // return "/";
     }
 }
