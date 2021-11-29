78960b7f0fed868b60b156418a26c166e7efcdcb
skolach <ssskolach@gmail.com>
2021-11-29 23:41:18 +0200

    user controller

--- src/main/java/com/epam/taxvicespring/controller/TaxviceController.java
+++ src/main/java/com/epam/taxvicespring/controller/TaxviceController.java
@@ -1,8 +1,14 @@
 package com.epam.taxvicespring.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.epam.taxvicespring.entity.User;
 import com.epam.taxvicespring.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +16,15 @@ public class TaxviceController {
 
     @Autowired
     UserRepository userRepository;
+
+    @GetMapping("/user")
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public Optional<User> oneUser(@PathVariable Integer id) {
+        return userRepository.findById(id);
+    }
 
 }
