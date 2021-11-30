dab7887f08d2f8903b5ef145ebb27914971822f6
skolach <ssskolach@gmail.com>
2021-11-30 16:44:02 +0200

    ...

--- src/main/java/com/epam/taxvicespring/TaxvicespringApplication.java
+++ src/main/java/com/epam/taxvicespring/TaxvicespringApplication.java
@@ -1,7 +1,13 @@
 package com.epam.taxvicespring;
 
+import java.util.List;
+
+import com.epam.taxvicespring.entity.User;
+import com.epam.taxvicespring.repository.UserRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -9,7 +15,17 @@ import org.springframework.context.annotation.ComponentScan;
 public class TaxvicespringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaxvicespringApplication.class, args);
+		System.out.println("");
+
+        ConfigurableApplicationContext cac = SpringApplication.run(TaxvicespringApplication.class, args);
+
+        for (String string : cac.getBeanDefinitionNames()) {
+            System.err.println(string);
+        }
+
+        List<User> users = cac.getBean(UserRepository.class).findAll();
+
+        System.out.println(users);
 	}
 
 }
